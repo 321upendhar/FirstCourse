33668f09458ff0ff92e9d90e1a4f0a0441aabf6d
upendhar vanam <upendharvanam231@gmail.com>
2020-08-18 18:47:05 +0530

    MY FIRST DAY COMMIT FROM TELUSUKO

---
+++ My Notebook Telusko 1.py
@@ -0,0 +1,61 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 10+2
+12
+>>> 12-2
+10
+>>> 6*2
+12
+>>> 20/4
+5.0
+>>> 5/2
+2.5
+>>> 5//2
+2
+>>> 8+9-10
+7
+>>> 8+9-
+SyntaxError: invalid syntax
+>>> 8+2*3
+14
+>>> (8+2)*3
+30
+>>> 2*2*2
+8
+>>> 2**3
+8
+>>>  10//3 to find quoficient
+ 
+SyntaxError: unexpected indent
+>>> 10//3
+3
+>>> 10%3
+1
+>>> 'navin'
+'navin'
+>>> print('navin')
+navin
+>>> print('navin's laptop')
+      
+SyntaxError: invalid syntax
+>>> print("navin's laptop")
+navin's laptop
+>>> print(''navin's "laptop"")
+      
+SyntaxError: invalid syntax
+>>> print('navin\'s "laptop"')
+navin's "laptop"
+>>> print('navin "laptop"')
+navin "laptop"
+>>> 'navin'+'navin'
+'navinnavin'
+>>> 10*'navin'
+'navinnavinnavinnavinnavinnavinnavinnavinnavinnavin'
+>>> print('c:\docs\navin')
+c:\docs
+avin
+>>> print(r'c:\docs\navin')avin
+SyntaxError: invalid syntax
+>>> print(r'c:\docs\navin')
+c:\docs\navin
+>>> 
